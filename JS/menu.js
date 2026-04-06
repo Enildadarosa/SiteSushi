a3564a1843ce78c1bbed18ef677bc669076b3cf9
ENILDA APARECIDA MENDES DA ROSA CACERES <enirosa4@hotmail.com>
2026-04-06 17:33:28 -0400

    Task01
    Criação do menu responsivo e da área main.

---
+++ JS/menu.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.mobile-btn').on('click',function(){
+        $('.mobile-menu').toggleClass('active');
+        $(this).find('i').toggleClass('fa-bars fa-x');
+    })
+})
+
